afd5954bbbe9175ca735e2c9da0a904c3f6b9e39
Marcela <msmelara@gmail.com>
2016-02-18 13:28:28 -0500

    changing the website

---
+++ temp_sensing/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml2/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Raspberry Pi Temperature App</title>
+</head>        
+<body>
+<h3>Raspberry Pi Temperature App</h3>
+         Current temperature in Sherrerd 320:
+<?php
+         //$data = file_get_contents('php://input');
+         //       $time = $data->{"time"};
+//$fahrenheit = $_POST["fahrenheit"];
+         //$celcius = $_POST["celcius"];
+         //echo $time.": ".$fahrenheit."&#176;F / ".$celcius."&#176;C"
+echo $_POST["time"]
+?>
+</body>
