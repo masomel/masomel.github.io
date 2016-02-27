59f97a709b56613e4abe91dc771e1721f3787aa6
Marcela <msmelara@gmail.com>
2016-02-26 22:45:00 -0500

    Added pelican-bootstrap3 theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -40,8 +40,6 @@ STATIC_PATHS = ['static/images']
   #  'static/style.css': {'path': 'static/style.css'}
 #}
 
-ABOUT_ME = '<p>I am a second-year PhD student in the Computer Science Department at Princeton University working with <a href=\"http://www.cs.princeton.edu/~mfreed\">Mike Freedman</a> and <a href=\"http://www.cs.princeton.edu/~felten\">Ed Felten</a>. In my research, I seek to build systems that provide strong security while operating in untrusted environments. <p> Previously, I recevied my M.S.E. in Computer Science from Princeton in June 2014, and was co-advised by Ed Felten and Mike Freedman. My Master\'s thesis was on <a href=\"http://coniks.org\">CONIKS</a>, a practical key management system for end users, which is run by untrusted key servers. <p> I received my B.S. in Computer Science, with a Minor in Physics from <a href=\"http://www.hws.edu\">Hobart and William Smith Colleges</a> (HWS) in Geneva, NY in May 2012. '
-
 AVATAR = 'static/images/marci.jpg'
 
 # Uncomment following line if you want document-relative URLs when developing
