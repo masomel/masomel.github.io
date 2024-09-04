bfe2ba1946b9c4bab97070452f5356d67cd25637
Marcela Melara <2728978+masomel@users.noreply.github.com>
2024-09-03 19:09:02 -0700

    Fix pkgs install
    Signed-off-by: Marcela Melara <2728978+masomel@users.noreply.github.com>

--- publish.sh
+++ publish.sh
@@ -5,10 +5,10 @@
 sudo apt-get install -y git make wget python3 python3-pip
 
 # install dependencies
-sudo pip3 install --upgrade pip
-sudo pip3 install --upgrade pelican[markdown]
-sudo pip3 install --upgrade ghp-import
-sudo pip3 install --upgrade Jinja2
+pip3 install --upgrade pip
+pip3 install --upgrade pelican[markdown]
+pip3 install --upgrade ghp-import
+pip3 install --upgrade Jinja2
 
 git submodule update --init
 
