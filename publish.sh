7a0e7206e7704289b7312526012c62e29e573c35
Marcela Melara <2728978+masomel@users.noreply.github.com>
2024-09-03 19:05:38 -0700

    Add pelican-plugins submodule
    Signed-off-by: Marcela Melara <2728978+masomel@users.noreply.github.com>

--- publish.sh
+++ publish.sh
@@ -10,13 +10,7 @@ sudo pip3 install --upgrade pelican[markdown]
 sudo pip3 install --upgrade ghp-import
 sudo pip3 install --upgrade Jinja2
 
-#wget https://github.com/masomel/pelican-themes/archive/refs/heads/main.zip -P /tmp
-#unzip /tmp/main.zip
-#mv pelican-themes-main pelican-themes
-
-#wget https://github.com/getpelican/pelican-plugins/archive/refs/heads/master.zip -P /tmp
-#unzip /tmp/master.zip
-#mv pelican-plugins-master pelican-plugins
+git submodule update --init
 
 mkdir -p output
 make html
